fe1eef95c395c15993ee5e101f207fe9c5e54297
Jose Quesada <jose.quesada@fernando3martos.com>
2024-12-16 08:28:56 +0100

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,20 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonModule } from 'primeng/button';
+import { SharedModule } from './shared/shared.module';
+
+import localeES from '@angular/common/locales/es'
+import localeFrCA from '@angular/common/locales/fr-CA'
+
+import { registerLocaleData } from '@angular/common';
+
+
+registerLocaleData(localeES);
+registerLocaleData(localeFrCA);
 
 @NgModule({
   declarations: [
@@ -10,9 +22,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    SharedModule,
+    BrowserAnimationsModule
+],
+  providers: [
+    {
+      provide: LOCALE_ID, useValue: 'es'
+    }
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
